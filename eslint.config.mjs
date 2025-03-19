492687dd0263fd63169551d7f02015abfdb925ed
olzhas-kalikhan <olzhas-kalikhan@outlook.com>
2025-03-19 16:07:37 +0500

    add eslint, prettier, and user model

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default defineConfig([
+  globalIgnores(["migrations", "node_modules"]),
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  eslintConfigPrettier,
+]);
